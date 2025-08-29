2972aa908cf5230efb309ef543589eae122b688c
gambadou <155756487+gambadou@users.noreply.github.com>
2025-08-29 22:24:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec tsun-gen3-proxy --mqtt-host "$MQTT_HOST" --mqtt-port "$MQTT_PORT" --mqtt-user "$MQTT_USER" --mqtt-password "$MQTT_PASSWORD"
+exec python -m tsun_proxy --mqtt-host "$MQTT_HOST" --mqtt-port "$MQTT_PORT" --mqtt-user "$MQTT_USER" --mqtt-password "$MQTT_PASSWORD"
